62cbd1f1e3d657fd9e815c67fdfaa7b4b1508b7f
mike8385 <146226367+mike8385@users.noreply.github.com>
2025-09-09 11:39:58 -0400

    Lab 1 complete

---
+++ app/src/main/java/com/example/simplecounterlab1/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.simplecounterlab1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+        var counter = 0
+         button.setOnClickListener {
+                 Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+                 counter++
+                 textView.text = counter.toString()
+
+         }
+
+    }
+}
